429a6eb32e713ccaa54e1bcc8fca4434bc441573
lukesaltweather <lukesaltweather@gmail.com>
2020-06-04 23:51:49 +0200

    setuppy

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'lukesaltweather',                   # Type in your name
   author_email = 'lukesaltweather@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/lukesaltweather/aiomangadex',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/lukesaltweather/aiomangadex/archive/0.1.0.tar.gz',    # I explain this later on
   keywords = ['Manga', 'API'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'aiohttp',
@@ -21,6 +21,6 @@ setup(
     'License :: OSI Approved :: Apache Software License', 
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.8'
   ],
 )
