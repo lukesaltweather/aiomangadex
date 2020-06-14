4c9ed254002e1657089f122b41e4618c1ce89205
Andreas Lengl <andilengl@outlook.de>
2020-06-14 19:52:36 +0200

    fix imports

--- aiomangadex/__init__.py
+++ aiomangadex/__init__.py
@@ -1,1 +1,3 @@
-from aiomangadex import Manga, Chapter, Language, fetch_manga
+from aiomangadex.aiomangadex import Manga, fetch_chapter, fetch_manga
+from aiomangadex.chapter import Chapter, ChapterList
+from aiomangadex.language import Language
