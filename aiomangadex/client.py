d11164eb099682f288b56b9073d40ada91f31932
lukesaltweather <lukesaltweather@gmail.com>
2020-11-08 16:31:45 +0100

    fetch_manga

--- aiomangadex/client.py
+++ aiomangadex/client.py
@@ -2,6 +2,7 @@ import asyncio
 
 from .session import MangadexSession
 from .manga import Manga
+from .chapterlist import ChapterList
 
 class MangadexClient:
 
@@ -15,4 +16,9 @@ class MangadexClient:
     #implement cache here
     async def fetch_manga(self, manga_id: int, include_chapters=False):
         res = await self.http.get_manga(manga_id, include_partial_chapters=include_chapters)
-        return Manga()
+        manga_data = res.pop('manga', None)
+        partial_chapters = res.pop('chapters', None)
+        return Manga(chapters=ChapterList.from_partial_chapters(partial_chapters),**manga_data)
+
+    async def fetch_chapter(self):
+        pass
