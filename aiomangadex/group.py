a04c79c43104b69f6be9dd940078044d1bbeb3f4
lukesaltweather <lukesaltweather@gmail.com>
2020-11-18 22:53:06 +0100

    add group class

--- aiomangadex/group.py
+++ aiomangadex/group.py
@@ -1,5 +1,41 @@
+from collections import namedtuple
 from dataclasses import dataclass
+from datetime import date, datetime
+from typing import List
+
+from .base import MangadexBase
+from .language import Language
+
+
+PartialUser = namedtuple('PartialUser', ('id', 'name'))
 
 @dataclass(frozen=True)
-class Group:
-    pass
+class Group(MangadexBase):
+    id: int = None
+    name: str = None
+    altNames: str = None
+    language: Language = None
+    leader: PartialUser = None
+    members: List[PartialUser] = None
+    description: str = None
+    website: str = None
+    discord: str = None
+    ircServer: str = None
+    ircChannel: str = None
+    email: str = None
+    founded: date = None
+    likes: int = None
+    follows: int = None
+    views: int = None
+    chapters: int = None
+    threadId: int = None
+    threadPosts = None
+    isLocked: bool = None
+    isInactive: bool = None
+    delay: int = None
+    lastUpdated: datetime = None
+    banner: str = None
+
+    @classmethod
+    def from_json(cls, json, http):
+        pass
