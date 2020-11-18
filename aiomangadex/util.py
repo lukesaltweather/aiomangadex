76119c89f7ad387efa321abfb960a359486b9ff4
lukesaltweather <lukesaltweather@gmail.com>
2020-11-18 22:56:09 +0100

    add various helpers

--- aiomangadex/util.py
+++ aiomangadex/util.py
@@ -1,3 +1,12 @@
+import asyncio
+import io
+
+from collections import AsyncIterator
+from typing import Any, Awaitable
+from inspect import isawaitable
+
+import aiohttp
+
 async def _download_file(session: aiohttp.ClientSession, url: str) -> io.BytesIO:
     """Helper function for downloading.
 
@@ -11,3 +20,37 @@ async def _download_file(session: aiohttp.ClientSession, url: str) -> io.BytesIO
     async with session.get(url) as response:
         assert response.status == 200
         return io.BytesIO(await response.read())
+
+def always_await(o: Any) -> Awaitable:
+    if isawaitable(o):
+        return o
+    fut = asyncio.get_event_loop().create_future()
+    fut.set_result(o)
+    return fut
+
+def zip_folder():
+    pass
+
+class ChapterIterator:
+    def __init__(self, session, base_url, *, limit = 50):
+        self.limit = limit
+        self.current_page = 1
+        self.session = session
+        self.base_url = session
+        super().__init__()
+
+    async def next(self):
+        return self.session._request(Route())
+
+    def __anext__(self):
+        return self.next()
+
+    async def flatten(self):
+        ret = []
+        while True:
+            try:
+                i = await self.next()
+            except StopAsyncIteration as e:
+                return ret
+            else:
+                ret.append(i)
